60dd5e6141545a08a6adfd3878575b29eaef6c20
Szymon <96234081+estwestminimu@users.noreply.github.com>
2023-12-01 21:26:47 +0100

    day1

---
+++ day1/com.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#only c
+g++  $1.cpp -o $1.out -lm && ./$1.out
+
+
---
+++ day1/gold.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char c;
+    int i=0;
+    int sum=0;
+    int firstCheck=1;
+    int second;
+    while(true)
+    {
+    std::cin>>c;
+    if(c=='.')
+    {
+ std::cout<<sum<<"\n";
+        return 0;
+    }
+    if(firstCheck)
+    {
+        if(((int(c) - 48) <=9)&&((int(c) - 48) >=0))
+        {
+        sum+=((int(c) - 48)*10);
+        firstCheck=0;
+        } 
+    }
+    if(((int(c) - 48) <=9)&&((int(c) - 48) >=0))
+        {
+          second=(int(c) - 48);
+        } 
+    if(c==',')
+    {   
+        sum+=second;
+        firstCheck=1;
+    }
+
+    i++;
+    }
+    return 0;
+}
